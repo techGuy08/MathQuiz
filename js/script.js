3dd90dce91b993cb48119c0e42738429b0316be2
korro21 <42660934+korro21@users.noreply.github.com>
2020-10-27 01:11:10 +0100

    Timer added

--- js/script.js
+++ js/script.js
@@ -1,34 +1,13 @@
-Array.prototype.shuffle = function () {
-  var i = this.length,
-    j,
-    temp;
-  if (i == 0) return this;
-  while (--i) {
-    j = Math.floor(Math.random() * (i + 1));
-    temp = this[i];
-    this[i] = this[j];
-    this[j] = temp;
-  }
-  return this;
-};
 window.addEventListener("load", function () {
-  const mathQuizEl = document.getElementById("mathQuiz");
+  const mathQuizEl = document.querySelector("#mathQuiz .mathQuiz-body");
   const mathQuizModalEl = document.getElementById("mathQuiz-modal");
-  const QAData = [];
-  for (let i = 1; i <= 10; i++) {
-    let n1 = Math.floor(Math.random() * (5 * i)) + 1;
-    let n2 = Math.floor(Math.random() * (5 * i)) + 1;
-    let ans = n1 * n2;
-    let question = {
-      n1,
-      n2,
-      ans,
-    };
-    QAData.push(question);
-  }
+  const timerEl = document.querySelector(".mathQuiz-timer");
+  const QAData = generateQuestions(10);
+
   const progress = {
     question: 1,
     win: 0,
+    wrong: 0,
     miss: 0,
     points: 0,
   };
@@ -42,7 +21,8 @@ window.addEventListener("load", function () {
         AllAnswers.push(random);
       }
     }
-    AllAnswersHTML = AllAnswers.shuffle()
+    console.log(shuffleArray(AllAnswers));
+    AllAnswersHTML = shuffleArray(AllAnswers)
       .map((el) => `<button class="mathQuiz_answers-item">${el}</button>`)
       .join("");
     let QHTML = `
@@ -75,6 +55,7 @@ window.addEventListener("load", function () {
     );
     let index = progress.question - 1;
     let nextIndex = index + 1;
+    isTimer = false;
     let value = Number(e.target.innerHTML);
     if (QAData[index].ans === value) {
       e.target.classList.add("correct");
@@ -82,22 +63,34 @@ window.addEventListener("load", function () {
       progress.points += 2;
     } else {
       e.target.classList.add("wrong");
-      progress.miss++;
+      progress.wrong++;
     }
+    showNextBtnfor(progress.question);
     progress.question++;
+  }
+  function showNextBtnfor(n) {
+    const index = n - 1;
+    if (n < 1 || n > AllQuestionsEl.length) return false;
+    const nextBtn = AllQuestionsEl[index].querySelector(".mathQuiz-item_next");
+    const ActiveButtons = document.querySelectorAll(
+      ".mathQuiz-item.active .mathQuiz_answers-item"
+    );
     ActiveButtons.forEach((btn) => {
       btn.disabled = true;
     });
-    const nextBtn = AllQuestionsEl[index].querySelector(".mathQuiz-item_next");
     nextBtn.classList.add("active");
     nextBtn.onclick = function () {
       gotoQuestion(progress.question);
+      isTimer = true;
+      timerValue = 30;
+      updateTimer(timerValue);
     };
     if (index === AllQuestionsEl.length - 1) {
       nextBtn.innerHTML = "Check Results";
       nextBtn.onclick = function (e) {
         mathQuizModalEl.classList.add("active");
         mathQuizModalEl.querySelector(".Res-wins").innerHTML = progress.win;
+        mathQuizModalEl.querySelector(".Res-wrong").innerHTML = progress.wrong;
         mathQuizModalEl.querySelector(".Res-miss").innerHTML = progress.miss;
         mathQuizModalEl.querySelector(".Res-score").innerHTML =
           progress.points + " / " + QAData.length * 2;
@@ -116,10 +109,14 @@ window.addEventListener("load", function () {
   }
   function ResetQuiz() {
     progress.miss = 0;
+    progress.wrong = 0;
     progress.points = 0;
     progress.question = 1;
     progress.win = 0;
+    isTimer = true;
+    timerValue = 30;
     gotoQuestion(1);
+    updateTimer(timerValue);
     mathQuizModalEl.classList.remove("active");
     document.querySelectorAll(".mathQuiz_answers-item").forEach((btn) => {
       btn.classList.remove("correct");
@@ -134,4 +131,57 @@ window.addEventListener("load", function () {
   mathQuizModalEl.querySelector(".bg").onclick = function () {
     mathQuizModalEl.classList.remove("active");
   };
+  function generateQuestions(amount) {
+    const arr = [];
+    for (let i = 1; i <= amount; i++) {
+      let n1 = Math.floor(Math.random() * (5 * i)) + 1;
+      let n2 = Math.floor(Math.random() * (5 * i)) + 1;
+      let ans = n1 * n2;
+      let question = {
+        n1,
+        n2,
+        ans,
+      };
+      arr.push(question);
+    }
+    return arr;
+  }
+  function shuffleArray(arr) {
+    if (arr.length == 0) return arr;
+    for (let i = arr.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * i);
+      let temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+    return arr;
+  }
+  let timerValue = 30;
+  let isTimer = true;
+  updateTimer(timerValue);
+  let timerCounter = setInterval(function () {
+    if (isTimer) {
+      if (timerValue > 0) timerValue--;
+      updateTimer(timerValue);
+      if (timerValue === 0 && progress.question < QAData.length) {
+        progress.question++;
+        timerValue = 30;
+        progress.miss++;
+        gotoQuestion(progress.question);
+        updateTimer(timerValue);
+      } else if (timerValue === 0 && progress.question === QAData.length) {
+        isTimer = false;
+        progress.miss++;
+        showNextBtnfor(progress.question);
+      }
+    }
+  }, 1000);
+  function updateTimer(value) {
+    timerEl.innerHTML = value + "s";
+    if (timerValue <= 10) {
+      timerEl.classList.add("red");
+    } else {
+      timerEl.classList.remove("red");
+    }
+  }
 });
